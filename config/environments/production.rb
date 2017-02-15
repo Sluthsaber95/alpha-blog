d9657f00063b7b500a05c02ae626a5e9504acf1e
Rails Udemy Course <monky95223@gmail.com>
2017-02-15 23:44:53 +0000

    Attempt #2  successful push Heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,12 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -77,3 +83,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
